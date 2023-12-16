db4698f3b76d142f2ffa94d1600adc6210bd3696
thebigby10 <abidurrahman22@iut-dhaka.edu>
2023-12-16 14:49:55 +0600

    readeable code selection sort

--- selection_sort.c
+++ selection_sort.c
@@ -5,11 +5,14 @@
 
 void swap(int *x, int *y);
 void selectionSort(int *x,int arr_len);
+int find_min_val_ind(int *x, int start_ind, int end_ind);
 
 int main(){
     int x[] = {1,6,3,2,8,3,1,1,10,100,0,-1};
-    selectionSort(x,12); //Argument: reference of x and the length of the array
-    // for(int i=0;i<12;i++) printf("%d ", *(x+i)); 
+    int arr_len = 12;
+    selectionSort(x,arr_len); //Argument: reference of x and the length of the array
+    printf("\033[4mSorted Array:\033[0m\n");
+    for(int i=0;i<12;i++) printf("%d ", *(x+i)); 
     return 0;
 }
 
@@ -19,10 +22,16 @@ void swap(int *x, int *y){
     *y = temp;
 }
 
+int find_min_val_ind(int *x, int start_ind, int end_ind){
+    int min_val_ind = start_ind;
+    for(int i=start_ind+1;i<=end_ind;i++) if(*(x+i)<*(x+min_val_ind)) min_val_ind = i;
+    return min_val_ind;
+}
+
 void selectionSort(int *x,int arr_len){
     int stepCount = 0;
     for(int i=0;i<arr_len-1;i++){
-        int min_val_ind=i; // This determines the minimum value for the position
+        int min_val_ind = find_min_val_ind(x,i,arr_len-1); // This determines the index of the minimum value
         for(int j=i+1;j<arr_len;j++) if(*(x+j)<*(x+min_val_ind)) min_val_ind = j;
         //Printing
         printf("\033[4mStep %d:\n\033[0m",++stepCount);
@@ -47,3 +56,4 @@ void selectionSort(int *x,int arr_len){
         //Printing
     }
 }
+
